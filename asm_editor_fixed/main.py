e8e32fd3c9f419cec9a4a098aed2b4c29e32aa6a
Sharvesh.R.V <sharvesh10001@gmail.com>
2026-04-24 21:53:14 +0530

    Correct typo in comment in main.py
    Fix typo in comment about fetching files.

--- asm_editor_fixed/main.py
+++ asm_editor_fixed/main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+# Thi is used for fetching the files from the current directory .
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from src.editor_app import AsmEditorApp
 
